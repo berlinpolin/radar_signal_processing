e8f19fe7424bd6238b73638356858b82f2e976a4
berlinpolin <43314334+berlinpolin@users.noreply.github.com>
2025-02-23 16:48:40 +0800

    update axes values

--- load_radar_data.py
+++ load_radar_data.py
@@ -20,16 +20,21 @@ def main():
     radar_raw_data_cube_range_velocity_fft = \
         np.fft.fft2(radar_raw_data_cube)
     radar_raw_data_cube_range_velocity_fft = \
-        np.fft.fftshift(radar_raw_data_cube_range_velocity_fft)
+        np.fft.fftshift(radar_raw_data_cube_range_velocity_fft, axes=(1,))
     radar_raw_data_cube_range_velocity_fft_mag_avg_db = \
         10*np.log10(np.mean(np.abs(radar_raw_data_cube_range_velocity_fft)**2, axis=0))
     
-    freq_x = np.fft.fftshift(np.fft.fftfreq(int(num_samp_per_chirp)))  # Frequency values for y-axis
-    freq_y = np.fft.fftshift(np.fft.fftfreq(int(num_chirp_per_frame)))  # Frequency values for x-axis
+    freq_x = (np.fft.fftshift(np.fft.fftfreq(int(num_samp_per_chirp))) + 0.5) *\
+                 num_samp_per_chirp # Frequency values for y-axis
+    freq_y = np.fft.fftshift(np.fft.fftfreq(int(num_chirp_per_frame))) *\
+                    num_chirp_per_frame  # Frequency values for x-axis
     freq_x = freq_x*f_s*c_air/(num_samp_per_chirp*2*slew_rate)
     freq_y = freq_y*wavelength/(num_chirp_per_frame*2*chirp_duration)
 
-    plt.figure(figsize=(6,6))
+    print('Max. Range :', f_s*c_air/(2*slew_rate))
+    print('Max. Velocity :', wavelength/(4*chirp_duration))
+
+    plt.figure()
     plt.imshow(radar_raw_data_cube_range_velocity_fft_mag_avg_db, \
                extent=[freq_x[0], freq_x[-1], freq_y[0], freq_y[-1]])
     plt.colorbar(label="Magnitude (log scale)")
