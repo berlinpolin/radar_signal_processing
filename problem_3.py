7c3ef5176583ef3188166ed5b9bf304ce4116313
berlinpolin <berlinc.ece07g@nctu.edu.tw>
2025-03-13 09:25:52 +0800

    add manifold_matrix in problem_3.py

--- problem_3.py
+++ problem_3.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def steering_vec(
         wavelength, # m
-        r:np.ndarray, # distance, m
+        r:np.ndarray, # (num_element,) distance, m
         theta, # degree
         num_element
         ):
@@ -12,6 +12,17 @@ def steering_vec(
         a[n] = np.exp(-2j*np.pi*r[n]*np.sin(theta)/wavelength)
 
     return a
+
+def manifold_matrix(
+        wavelength, # m
+        r:np.ndarray, # (num_element,) distance, m
+        theta:np.ndarray, # degree
+        num_element
+        ):
+    num_theta = len(theta)
+    s = np.zeros((num_element, num_theta), dtype=np.complex_)
+    for n in range(num_theta):
+        s[:, n] = steering_vec(wavelength, r, theta[n], num_element)
 
 def main():
     s = np.loadtxt('data/problem_3/s_single_target.txt', dtype=np.complex_)
